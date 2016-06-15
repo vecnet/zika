cb61c0cb8bdbcb7730ea045b81c793ae305ae955
Alex Vyushkov <baguage@gmail.com>
2016-06-15 10:40:04 -0400

    added model for zika cases

--- website/apps/data_browser/models.py
+++ website/apps/data_browser/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class ZikaCasesColumbia(models.Model):
+    report_date = models.DateField()
+    location = models.TextField()
+    data_field_code = models.TextField()
+    data_type = models.TextField()
+    value = models.IntegerField()
+
+    class Meta:
+        db_table = "zika_cases"
--- website/apps/data_browser/views.py
+++ website/apps/data_browser/views.py
@@ -3,5 +3,27 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from website.apps.data_browser.models import ZikaCasesColumbia
+
+
 def hello(request):
+
+    new_case = ZikaCasesColumbia.objects.create(
+        report_date="1990-01-01",
+        location="Chicago",
+        data_field_code="CODE",
+        data_type="case",
+        value=1,
+    )
+
+    for case in ZikaCasesColumbia.objects.all():
+        if case.report_date is None:
+            case.report_date = "2006-04-05"
+        case.value = case.value + 1
+        case.save()
+        print "id: %s" % case.pk
+        print "report_date: %s" % case.report_date
+        print "location: " + case.location
+        print "value: %s " % case.value
+
     return HttpResponse(content="hello")
