f8db3324b1af23c11b5c2f79acb0696125c243ec
Alexander Vyushkov <baguage@gmail.com>
2016-08-12 17:18:21 -0400

    Fixed logout url for VecNet SSO

--- website/settings/prod.py
+++ website/settings/prod.py
@@ -41,6 +41,7 @@ SECURE_BROWSER_XSS_FILTER = True
 
 # VecNet Single Sign On integration
 LOGIN_URL = '/sso/'
+LOGOUT_URL="https://www.vecnet.org/index.php/log-out"
 TKT_AUTH_LOGIN_URL = 'https://www.vecnet.org/index.php/sso-login'
 TKT_AUTH_PUBLIC_KEY = os.path.join(BASE_DIR, 'apache', 'tkt_pubkey_dsa.pem')
 
