c2f9c7b41e20c5b2cd6f8c38a7b9168762a136eb
Alexander Vyushkov <baguage@gmail.com>
2016-08-13 09:53:05 -0400

    Updated copyright statements

--- website/apps/simulation/admin.py
+++ website/apps/simulation/admin.py
@@ -1,11 +1,18 @@
-# Copyright (C) 2016, University of Notre Dame
-# All rights reserved
-
+#!/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# This file is part of the VecNet Zika modeling interface.
+# For copyright and licensing information about this package, see the
+# NOTICE.txt and LICENSE.txt files in its top-level directory; they are
+# available at https://github.com/vecnet/zika
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License (MPL), version 2.0.  If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from website.apps.simulation.models import Location, Simulation, Data
-
-
 from django.contrib import admin
+
 
 admin.site.register(Location)
 admin.site.register(Simulation)
--- website/apps/simulation/views/BrowseView.py
+++ website/apps/simulation/views/BrowseView.py
@@ -1,5 +1,15 @@
-# Copyright (C) 2016, University of Notre Dame
-# All rights reserved
+#!/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# This file is part of the VecNet Zika modeling interface.
+# For copyright and licensing information about this package, see the
+# NOTICE.txt and LICENSE.txt files in its top-level directory; they are
+# available at https://github.com/vecnet/zika
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License (MPL), version 2.0.  If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
