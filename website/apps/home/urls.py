e40789d54f7381ce8d2d5522c8db7012fa09e0e1
haroldfry <bshen@nd.edu>
2016-10-03 14:01:22 -0400

    relink browse map to dropdown menu page

--- website/apps/home/urls.py
+++ website/apps/home/urls.py
@@ -13,7 +13,6 @@
 from django.conf.urls import url
 from website.apps.home.views import load_locations
 from website.apps.home.views import load_municipality
-from website.apps.home.views import testview
 from website.apps.home.views import testmap
 from website.apps.home.views import testchoropleth
 from website.apps.home.views import load_examplecsv
@@ -23,13 +22,20 @@ from website.apps.home.views import csvfake
 
 
 urlpatterns = [
-    #url(r'^$', testview),
     url(r'^$', testmap, name="home.map"),
     url(r'^testchoropleth/$', testchoropleth, name='choropleth_map'),
     url(r'^loadexamplecsv/$', load_examplecsv),
+
+    # hard to get to this page
     url(r'^municipality/(?P<department_name>[A-Z, a-z, _]+)/(?P<municipality_name>[A-Z, a-z, _]+)/$', load_municipality),
+
     url(r'^dropdnegcsv/$', dropdown_menu, name='home.dropdown'),
+
+    # just choropleth map and highchart
     url(r'^choroplethdetail/(?P<inquery_date>[0-9, -]+)/$', detailchoropleth),
+
     url(r'^csvfake/(?P<inquery_date>[0-9, -]+)/$', csvfake, name='csvfake'),
+
+    # department infor using highchart, example: http://127.0.0.1:8000/home/CESAR/
     url(r'^(?P<department_name>[A-Z, a-z, _]+)/$', load_locations),
 ]
