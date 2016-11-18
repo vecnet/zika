e45326bdf484a19d29c358a64324499c9d425180
Elizabeth <ecaldwe1@nd.edu>
2016-11-18 14:56:19 -0500

    map works

--- website/apps/home/views/MapView.py
+++ website/apps/home/views/MapView.py
@@ -1,3 +1,5 @@
+from django.template import loader
+from django.core.urlresolvers import reverse
 from django.views.generic.base import TemplateView
 
 from website.apps.simulation.models import Data
@@ -13,12 +15,26 @@ class MapView(TemplateView):
         info = Data.objects.filter(location__municipality_code='05001', simulation_id=sim_id).values('date')
 
         date_info = []
+
         for d in info:
             date_info.append(str(d['date']))
 
+        date_arg = None
+        if (kwargs.get('inquery_date')):
+            date_arg = kwargs.get('inquery_date')
+        else:
+            date_arg = date_info[-1]
+
+        # Coped from choropleth_map_view
+        passjspath = reverse('home.csv_for_map', kwargs={"inquery_date": date_arg, "sim_id": sim_id})
+        #template = loader.get_template('home/choropleth_map.html')
+        # context = {'generatefilepath': passjspath, 'inquery_date': date_arg, 'sim_id': sim_id}
+
         context = {
+            "date_arg": date_arg,
             "date_list": date_info,
-            "sim_id": sim_id
+            "sim_id": sim_id,
+            'generatefilepath': passjspath
         }
         return context
 
--- website/static/map.js
+++ website/static/map.js
@@ -5,21 +5,110 @@
  */
 		$(document).ready(function() {
 
-            // document.getElementById("date-dropdown").onchange = function() {
-            //     window.alert(document.getElementById("picked-date").value);
-            //     document.getElementById("picked-date").value = document.getElementById("date-d;ropdown").value
-            //     // dateSelected();
-            //     // window.alert(document.getElementById("date-dropdown").value);
-            // };
+            //Width and height
+            var w = 1000;
+            var h = 600;
 
+            //Define map projection
+            var projection = d3.geo.albersUsa()
+                    .translate([0, -350])
+                    .scale([1000]);
+
+            //Define path generator
+            var path = d3.geo.path()
+                    .projection(projection);
+
+            //Define quantize scale to sort data values into buckets of color
+            var color = d3.scale.quantize()
+                    .range(["rgb(237,248,233)", "rgb(186,228,179)", "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"]);
+            //Colors taken from colorbrewer.js, included in the D3 download
+
+            //Create SVG element
+            var div = d3.select("body").append("div")
+                    .attr("class", "tooltip")
+                    .style("opacity", 0);
+            var svg = d3.select("body").select("svg");
+
+            //Load in zika choropleth map data
+            var csvpath = $("#csv-url").attr("data-url"); //"{{ generatefilepath }}";
+            window.alert(csvpath);
+
+            d3.csv(csvpath, function (data) {
+                //Set input domain for color scale
+                color.domain([
+                    d3.min(data, function (d) {
+                        return d.value;
+                    }),
+                    d3.max(data, function (d) {
+                        return d.value;
+                    })
+                ]);
+
+                //Load in GeoJSON data
+                d3.json("/static/examples/colombia_admin3.json", function (json) {
+                    //Merge the ag. data and GeoJSON
+                    //Loop through once for each ag. data value
+                    for (var i = 0; i < data.length; i++) {
+                        //Grab state name
+                        var dataState = data[i].ID_ESPACIA;
+                        //Grab data value, and convert from string to float
+                        var dataValue = data[i].value;
+
+                        //Find the corresponding state inside the GeoJSON
+                        for (var j = 0; j < json.features.length; j++) {
+
+                            var jsonState = json.features[j].properties.ID_ESPACIA;
+
+                            if (dataState == jsonState) {
+                                //Copy the data value into the JSON
+                                json.features[j].properties.value = dataValue;
+                                //Stop looking through the JSON
+                                break;
+
+                            }
+                        }
+                    }
+
+                    //Bind data and create one path per GeoJSON feature
+                    svg.selectAll("path")
+                            .data(json.features)
+                            .enter()
+                            .append("path")
+                            .attr("d", path)
+                            .style("fill", function (d) {
+                                //Get data value
+                                var value = d.properties.value;
+                                if (value) {
+                                    //If value exists…
+                                    return color(value);
+                                } else {
+                                    //If value is undefined…
+                                    return "#ccc";
+                                }
+                            })
+                            .on("click", function(d){
+                                var simulationumber = "{{ sim_id }}";
+                                var url = $("#url").attr("data-url");
+                                url += simulationumber;
+                                url += "/";
+                                url += d.properties.ID_ESPACIA;
+                                document.getElementById('iframe2').src = url;
+                            })
+                            .on("mouseover", function(d) {
+                                d3.select(this).transition().duration(300).style("opacity", 1);
+                                div.transition().duration(300)
+                                        .style("opacity", 1)
+                                div.text(d.properties.NOM_DEPART + " " + d.properties.NOM_MUNICI + " " + d.properties.value)
+                                        .style("left", (d3.event.pageX) + "px")
+                                        .style("top", (d3.event.pageY -30) + "px")
+                            })
+                            .on("mouseout", function() {
+                                d3.select(this)
+                                        .transition().duration(300)
+                                        .style("opacity", 0.8);
+                                div.transition().duration(300)
+                                        .style("opacity", 0);
+                            })
+                });
+            });
         });
-
-
-// Function t
-function dateSelected() {
-    // window.alert(select_value);
-    var select_value = document.getElementById("date-dropdown").value;
-    // window.alert(select_value);
-    // var show_picked_date = document.getElementById("picked-date");
-    document.getElementById("picked-date").value = select_value;
-}
