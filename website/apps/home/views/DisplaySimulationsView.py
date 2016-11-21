2a443fd1e61f6c143a8ca69d7e7a650048c866af
Elizabeth <ecaldwe1@nd.edu>
2016-11-21 14:13:47 -0500

    Added new file for simulation table view, updated index to be about in preparation for changing front page, deleted unused templates

---
+++ website/apps/home/views/DisplaySimulationsView.py
@@ -0,0 +1,32 @@
+from django.http.response import HttpResponse
+from django.template import loader
+
+from website.apps.simulation.models import Simulation
+
+
+# View for the table of simulations and models (at /home url)
+def display_simulations(request):
+
+    # Get all the simulations
+    all_simulations = Simulation.objects.all()
+
+    sim_list = []
+
+    # For each simulation in all_simulations
+    for entry in all_simulations:
+        sim = {
+            'simulation_id': str(entry.id),
+            'simulation_name': entry.name,
+            'model_name': entry.model_name,
+            'create_time': entry.creation_timestamp
+        }
+
+        # Add this simulation entry to the list
+        sim_list.append(sim)
+
+    template = loader.get_template('home/simulation.html')
+    context = {
+        'simulation_model_list': sim_list
+    }
+
+    return HttpResponse(template.render(context, request))
--- website/urls.py
+++ website/urls.py
@@ -17,11 +17,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
-from website.apps.home.views import IndexView
+from website.apps.home.views import AboutView
 
 urlpatterns = [
+    url(r'^$', AboutView.as_view(), name="index"),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^about/$', AboutView.as_view(), name="about"),
     # robots.txt is implemented as a template because Django can't seem to serve a static file from urls.py
     url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt")),
     # Please refer to https://docs.djangoproject.com/en/1.8/topics/auth/default/#using-the-views
