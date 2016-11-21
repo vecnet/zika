bec5becba35766382a92681735d2cc5da7447921
Elizabeth <ecaldwe1@nd.edu>
2016-11-21 14:45:47 -0500

    add arrow buttons to date dropdown

--- website/apps/home/static/map.js
+++ website/apps/home/static/map.js
@@ -96,10 +96,10 @@
                             .on("mouseover", function(d) {
                                 d3.select(this).transition().duration(300).style("opacity", 1);
                                 div.transition().duration(300)
-                                        .style("opacity", 1)
+                                        .style("opacity", 1);
                                 div.text(d.properties.NOM_DEPART + " " + d.properties.NOM_MUNICI + " " + d.properties.value)
                                         .style("left", (d3.event.pageX) + "px")
-                                        .style("top", (d3.event.pageY -30) + "px")
+                                        .style("top", (d3.event.pageY -30) + "px");
                             })
                             .on("mouseout", function() {
                                 d3.select(this)
@@ -111,3 +111,4 @@
                 });
             });
         });
+
