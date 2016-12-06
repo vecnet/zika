acf8c7669939f41f7fb47a6d18e043923232c8a8
Elizabeth <ecaldwe1@nd.edu>
2016-12-06 15:38:49 -0500

    uncommented stuff

--- scripts/get_dept_municipalities.py
+++ scripts/get_dept_municipalities.py
@@ -3,7 +3,7 @@
 # data_cases_combo_new.csv and writes them to a file.
 
 data_file = open('../website/apps/home/data/data_cases_combo_new.csv', 'r')
-# write_file = open('department_municipalities_colombia.csv', 'w')
+write_file = open('all_department_municipalities_colombia.csv', 'w')
 
 all_dept_municipality_list = []
 date_list = []
@@ -18,6 +18,6 @@ for line in data_file:
 dept_mun_set = set(all_dept_municipality_list)
 all_dept_municipality_list = list(dept_mun_set)
 
-# for item in all_dept_municipality_list:
-#     write_file.write(item+"\n")
+for item in all_dept_municipality_list:
+    write_file.write(item+"\n")
 print(list(set(date_list)))
