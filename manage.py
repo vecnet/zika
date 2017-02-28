8b33f8b00ee0785843f4cad732bfd281ce1dacf3
Alexander Vyushkov <baguage@gmail.com>
2017-02-28 10:52:05 -0500

    Backported code for specifying settings file from OpenMalaria portal

--- manage.py
+++ manage.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
+try:
+    from config_local import settings_module
+    settings_module_message = "as defined in config_local.py file. DJANGO_SETTINGS_MODULE is ignored."
+except ImportError:
+    settings_module = "website.settings.dev"
+    settings_module_message = ""
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
     from django.core.management import execute_from_command_line
-
+    print("manage.py: Using settings: %s %s" % (os.environ.get("DJANGO_SETTINGS_MODULE"), settings_module_message))
     execute_from_command_line(sys.argv)
