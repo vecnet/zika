f1f222c76662cd5f9821527c12381bb0ae86ffc5
Alexander Vyushkov <baguage@gmail.com>
2017-03-23 17:48:59 -0400

    Admin panel improvements

--- website/apps/home/admin.py
+++ website/apps/home/admin.py
@@ -14,8 +14,24 @@ from django.contrib import admin
 
 from website.apps.home.models import Location, Simulation, Data, SimulationModel, Totals
 
-admin.site.register(Location)
-admin.site.register(Simulation)
-admin.site.register(Data)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("id", "department", "department_code", "municipality", "municipality_code", "creation_timestamp")
+    ordering = ("department", "municipality")
+    search_fields = ("department", "department_code", "municipality", "municipality_code")
+admin.site.register(Location, LocationAdmin)
+
+class SimulationAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "disease", "historical", "is_uploaded", "created_by", "creation_timestamp")
+    ordering = ("-creation_timestamp",)
+    search_fields = ("name", "created_by__username")
+admin.site.register(Simulation, SimulationAdmin)
+
+class DataAdmin(admin.ModelAdmin):
+    list_display = ("location", "simulation", "value_low", "value_mid", "value_high", "date")
+    ordering = ("-simulation",)
+    search_fields = ("simulation__name", "location__department", "location__municipality", "simulation__created_by__username")
+    list_filter = ("simulation", "location__department")
+admin.site.register(Data, DataAdmin)
+
 admin.site.register(Totals)
 admin.site.register(SimulationModel)
