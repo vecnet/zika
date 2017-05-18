692f2393675aabb14f9f29bdc0abf8f0731ae8a9
Alexander Vyushkov <baguage@gmail.com>
2017-05-18 11:57:38 -0400

    - Fixed tests - Added warning on home page if there are no simulations in the system

--- website/apps/home/views/__init__.py
+++ website/apps/home/views/__init__.py
@@ -9,7 +9,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License (MPL), version 2.0.  If a copy of the MPL was not distributed
 # with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic.base import TemplateView
 
 from website.apps.home.models import Simulation
@@ -20,10 +20,13 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         # Get the most recent simulation so that clicking the Map View button will go to the appropriate page
-        latest_simulation = Simulation.objects.exclude(historical=True).latest('creation_timestamp')
-        sim_id = latest_simulation.id
-        model_id = latest_simulation.sim_model.id
-
+        try:
+            latest_simulation = Simulation.objects.exclude(historical=True).latest('creation_timestamp')
+            sim_id = latest_simulation.id
+            model_id = latest_simulation.sim_model.id
+        except ObjectDoesNotExist:
+            sim_id = None
+            model_id = None
         context = {
             "most_recent_sim_id": sim_id,
             "most_recent_model_id": model_id,
